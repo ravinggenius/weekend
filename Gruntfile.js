8ffc39bbef50b119455f0b2e6445fb0524f3c780
Thomas Ingram <code@ravinggenius.com>
2013-09-27 23:09:52 -0400

    Use tabs for indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
 var mountFolder = function (connect, dir) {
-    return connect.static(require('path').resolve(dir));
+	return connect.static(require('path').resolve(dir));
 };
 
 // # Globbing
@@ -13,268 +13,268 @@ var mountFolder = function (connect, dir) {
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
-    // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	// load all grunt tasks
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
-        watch: {
-            compass: {
-                files: [
-                    'styles/{,*/}*.css',
-                    'styles/{,*/}*.{scss,sass}'
-                ],
-                tasks: ['compass:server']
-            },
-            livereload: {
-                options: {
-                    livereload: LIVERELOAD_PORT
-                },
-                files: [
-                    '*.html',
-                    '{.tmp,./}/styles/{,*/}*.css',
-                    '{.tmp,./}/js/{,*/}*.js',
-                    'img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-                ]
-            }
-        },
-        connect: {
-            options: {
-                port: 9000,
-                // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
-            },
-            livereload: {
-                options: {
-                    middleware: function (connect) {
-                        return [
-                            lrSnippet,
-                            mountFolder(connect, '.tmp'),
-                            mountFolder(connect, '.')
-                        ];
-                    }
-                }
-            },
-            dist: {
-                options: {
-                    middleware: function (connect) {
-                        return [
-                            mountFolder(connect, 'build')
-                        ];
-                    }
-                }
-            }
-        },
-        open: {
-            server: {
-                path: 'http://localhost:<%= connect.options.port %>'
-            }
-        },
-        clean: {
-            dist: {
-                files: [
-                    {
-                        dot: true,
-                        src: [
-                            '.tmp',
-                            'build/*',
-                            '!build/.git*'
-                        ]
-                    }
-                ]
-            },
-            server: '.tmp'
-        },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            all: [
-                'Gruntfile.js',
-                'js/{,*/}*.js'
-            ]
-        },
-        compass: {
-            options: {
-                sassDir: 'styles',
-                cssDir: '.tmp/styles',
-                generatedImagesDir: '.tmp/img/generated',
-                imagesDir: 'img',
-                javascriptsDir: 'js',
-                fontsDir: 'styles/fonts',
-                importPath: 'components',
-                httpImagesPath: '/img',
-                httpGeneratedImagesPath: '/img/generated',
-                relativeAssets: true,
-                outputStyle: 'compressed',
-                noLineComments: true
-            },
-            dist: {},
-            server: {
-                options: {
-                    debugInfo: true
-                }
-            }
-        },
-        // not used since Uglify task does concat,
-        // but still available if needed
-        /*concat: {
-            dist: {}
-        },*/
-        // not enabled since usemin task does concat and uglify
-        // check index.html to edit your build targets
-        // enable this task if you prefer defining your build targets here
-        /*uglify: {
-            dist: {}
-        },*/
-        rev: {
-            dist: {
-                files: {
-                    src: [
-                        'build/js/{,*/}*.js',
-                        'build/styles/{,*/}*.css',
-                        'build/img/{,*/}*.{png,jpg,jpeg,gif,webp}',
-                        'build/styles/fonts/*'
-                    ]
-                }
-            }
-        },
-        useminPrepare: {
-            options: {
-                dest: 'build'
-            },
-            html: 'index.html'
-        },
-        usemin: {
-            options: {
-                dirs: ['build']
-            },
-            html: ['build/{,*/}*.html'],
-            css: ['build/styles/{,*/}*.css']
-        },
-        imagemin: {
-            dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'img',
-                        src: '{,*/}*.{png,jpg,jpeg}',
-                        dest: 'build/img'
-                    }
-                ]
-            }
-        },
-        svgmin: {
-            dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'img',
-                        src: '{,*/}*.svg',
-                        dest: 'build/img'
-                    }
-                ]
-            }
-        },
-        cssmin: {
-            dist: {
-                files: {
-                    'build/styles/main.css': [
-                        '.tmp/styles/{,*/}*.css'
-                    ]
-                }
-            }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                    /*removeCommentsFromCDATA: true,
-                    // https://github.com/yeoman/grunt-usemin/issues/44
-                    //collapseWhitespace: true,
-                    collapseBooleanAttributes: true,
-                    removeAttributeQuotes: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeOptionalTags: true*/
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: './',
-                        src: '*.html',
-                        dest: 'build'
-                    }
-                ]
-            }
-        },
-        // Put files not handled in other tasks here
-        copy: {
-            dist: {
-                files: [
-                    {
-                        expand: true,
-                        dot: true,
-                        cwd: './',
-                        dest: 'build',
-                        src: [
-                            '*.{ico,txt}',
-                            '.htaccess',
-                            'img/{,*/}*.{webp,gif}',
-                            'styles/fonts/*'
-                        ]
-                    },
-                    {
-                        expand: true,
-                        cwd: '.tmp/img',
-                        dest: 'build/img',
-                        src: [
-                            'generated/*'
-                        ]
-                    }
-                ]
-            }
-        },
-        concurrent: {
-            server: [
-                'compass:server'
-            ],
-            dist: [
-                'compass:dist',
-                'imagemin',
-                'svgmin',
-                'htmlmin'
-            ]
-        }
-    });
+	grunt.initConfig({
+		watch: {
+			compass: {
+				files: [
+					'styles/{,*/}*.css',
+					'styles/{,*/}*.{scss,sass}'
+				],
+				tasks: ['compass:server']
+			},
+			livereload: {
+				options: {
+					livereload: LIVERELOAD_PORT
+				},
+				files: [
+					'*.html',
+					'{.tmp,./}/styles/{,*/}*.css',
+					'{.tmp,./}/js/{,*/}*.js',
+					'img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+				]
+			}
+		},
+		connect: {
+			options: {
+				port: 9000,
+				// change this to '0.0.0.0' to access the server from outside
+				hostname: 'localhost'
+			},
+			livereload: {
+				options: {
+					middleware: function (connect) {
+						return [
+							lrSnippet,
+							mountFolder(connect, '.tmp'),
+							mountFolder(connect, '.')
+						];
+					}
+				}
+			},
+			dist: {
+				options: {
+					middleware: function (connect) {
+						return [
+							mountFolder(connect, 'build')
+						];
+					}
+				}
+			}
+		},
+		open: {
+			server: {
+				path: 'http://localhost:<%= connect.options.port %>'
+			}
+		},
+		clean: {
+			dist: {
+				files: [
+					{
+						dot: true,
+						src: [
+							'.tmp',
+							'build/*',
+							'!build/.git*'
+						]
+					}
+				]
+			},
+			server: '.tmp'
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'js/{,*/}*.js'
+			]
+		},
+		compass: {
+			options: {
+				sassDir: 'styles',
+				cssDir: '.tmp/styles',
+				generatedImagesDir: '.tmp/img/generated',
+				imagesDir: 'img',
+				javascriptsDir: 'js',
+				fontsDir: 'styles/fonts',
+				importPath: 'components',
+				httpImagesPath: '/img',
+				httpGeneratedImagesPath: '/img/generated',
+				relativeAssets: true,
+				outputStyle: 'compressed',
+				noLineComments: true
+			},
+			dist: {},
+			server: {
+				options: {
+					debugInfo: true
+				}
+			}
+		},
+		// not used since Uglify task does concat,
+		// but still available if needed
+		/*concat: {
+			dist: {}
+		},*/
+		// not enabled since usemin task does concat and uglify
+		// check index.html to edit your build targets
+		// enable this task if you prefer defining your build targets here
+		/*uglify: {
+			dist: {}
+		},*/
+		rev: {
+			dist: {
+				files: {
+					src: [
+						'build/js/{,*/}*.js',
+						'build/styles/{,*/}*.css',
+						'build/img/{,*/}*.{png,jpg,jpeg,gif,webp}',
+						'build/styles/fonts/*'
+					]
+				}
+			}
+		},
+		useminPrepare: {
+			options: {
+				dest: 'build'
+			},
+			html: 'index.html'
+		},
+		usemin: {
+			options: {
+				dirs: ['build']
+			},
+			html: ['build/{,*/}*.html'],
+			css: ['build/styles/{,*/}*.css']
+		},
+		imagemin: {
+			dist: {
+				files: [
+					{
+						expand: true,
+						cwd: 'img',
+						src: '{,*/}*.{png,jpg,jpeg}',
+						dest: 'build/img'
+					}
+				]
+			}
+		},
+		svgmin: {
+			dist: {
+				files: [
+					{
+						expand: true,
+						cwd: 'img',
+						src: '{,*/}*.svg',
+						dest: 'build/img'
+					}
+				]
+			}
+		},
+		cssmin: {
+			dist: {
+				files: {
+					'build/styles/main.css': [
+						'.tmp/styles/{,*/}*.css'
+					]
+				}
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					/*removeCommentsFromCDATA: true,
+					// https://github.com/yeoman/grunt-usemin/issues/44
+					//collapseWhitespace: true,
+					collapseBooleanAttributes: true,
+					removeAttributeQuotes: true,
+					removeRedundantAttributes: true,
+					useShortDoctype: true,
+					removeEmptyAttributes: true,
+					removeOptionalTags: true*/
+				},
+				files: [
+					{
+						expand: true,
+						cwd: './',
+						src: '*.html',
+						dest: 'build'
+					}
+				]
+			}
+		},
+		// Put files not handled in other tasks here
+		copy: {
+			dist: {
+				files: [
+					{
+						expand: true,
+						dot: true,
+						cwd: './',
+						dest: 'build',
+						src: [
+							'*.{ico,txt}',
+							'.htaccess',
+							'img/{,*/}*.{webp,gif}',
+							'styles/fonts/*'
+						]
+					},
+					{
+						expand: true,
+						cwd: '.tmp/img',
+						dest: 'build/img',
+						src: [
+							'generated/*'
+						]
+					}
+				]
+			}
+		},
+		concurrent: {
+			server: [
+				'compass:server'
+			],
+			dist: [
+				'compass:dist',
+				'imagemin',
+				'svgmin',
+				'htmlmin'
+			]
+		}
+	});
 
-    grunt.registerTask('server', function (target) {
-        if (target === 'dist') {
-            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
-        }
+	grunt.registerTask('server', function (target) {
+		if (target === 'dist') {
+			return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+		}
 
-        grunt.task.run([
-            'clean:server',
-            'concurrent:server',
-            'connect:livereload',
-            'open',
-            'watch'
-        ]);
-    });
+		grunt.task.run([
+			'clean:server',
+			'concurrent:server',
+			'connect:livereload',
+			'open',
+			'watch'
+		]);
+	});
 
-    grunt.registerTask('build', [
-        'clean:dist',
-        'useminPrepare',
-        'concurrent:dist',
-        'cssmin',
-        'concat',
-        'uglify',
-        'copy',
-        'rev',
-        'usemin'
-    ]);
+	grunt.registerTask('build', [
+		'clean:dist',
+		'useminPrepare',
+		'concurrent:dist',
+		'cssmin',
+		'concat',
+		'uglify',
+		'copy',
+		'rev',
+		'usemin'
+	]);
 
-    grunt.registerTask('default', [
-        'jshint',
-        'build'
-    ]);
+	grunt.registerTask('default', [
+		'jshint',
+		'build'
+	]);
 };
