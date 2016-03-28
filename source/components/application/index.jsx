b9ecd1912c7002abb3523ab9e7e1e7d12dfe68b1
Thomas Ingram <code@ravinggenius.com>
2016-03-28 19:55:03 -0400

    Scale color stops between white and black, inclusive
    Also add classes to optimize foreground contrast

--- source/components/application/index.jsx
+++ source/components/application/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const isDark = function (red, green, blue) {
+	const strength = (red * 299) + (green * 587) + (blue * 114);
+	return (strength / 1000) < 128;
+};
+
 // http://stackoverflow.com/a/9744576
 const padLeft = function (number, padLength, padChar) {
 	padChar = (typeof padChar !== 'undefined') ? padChar : '0';
@@ -7,12 +12,22 @@ const padLeft = function (number, padLength, padChar) {
 	return (pad + number).slice(-Math.max(pad.length, number.toString().length));
 };
 
+const scale = function (number, fromMax) {
+	const toMax = 255;
+	return Math.round(number * toMax / fromMax);
+};
+
 export default class extends React.Component {
 	render() {
 		const isWeekend = this.props.isWeekend;
 		const { hours, minutes, seconds } = this.props.timeRemaining;
 
-		document.body.style.backgroundColor = `rgb(${hours}, ${minutes}, ${seconds})`;
+		const red = scale(hours, isWeekend ? 62 : 104);
+		const green = scale(minutes, 59);
+		const blue = scale(seconds, 59);
+
+		document.body.className = (isDark(red, green, blue) ? 'is-dark' : 'is-light');
+		document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 		return <article>
 			<section className="answer">{isWeekend ? 'Yes' : 'No'}</section>
