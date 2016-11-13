56ff797cd857faee2c97fdf15e7d8b4ecd1a796b
Thomas Ingram <code@ravinggenius.com>
2016-11-12 23:43:46 -0500

    Tighten up footer

--- app/components/Credits/index.js
+++ app/components/Credits/index.js
@@ -4,6 +4,9 @@ import styles from './styles.css';
 
 export default function () {
   return <footer className={styles.credits}>
-    <small className={styles.copyright}>© <a href="http://www.ravinggenius.com/">Thomas Ingram</a></small>
+    <small className={styles.copyright}>
+      ©&nbsp;
+      <a href="http://www.ravinggenius.com/">Thomas Ingram</a>
+    </small>
   </footer>;
 }
