5f0c8f50aa4f6244845cb522cb2044ce513911e1
Thomas Ingram <code@ravinggenius.com>
2016-11-13 00:07:43 -0500

    Tweak answer texts

--- app/components/Answer/index.js
+++ app/components/Answer/index.js
@@ -4,7 +4,7 @@ import styles from './styles.css';
 
 export default function Answer(props) {
   const className = props.isWeekend ? styles.answerYes : styles.answerNo;
-  const answer = props.isWeekend ? 'Yes' : 'Nope';
+  const answer = props.isWeekend ? 'Yes!' : 'nope';
   return <section className={className}>{answer}</section>;
 }
 
