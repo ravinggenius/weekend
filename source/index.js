77d87861c9c5df8f1d52329c3756865629a1dd82
Thomas Ingram <code@ravinggenius.com>
2018-06-11 19:17:45 -0400

    Render more efficiently
    Rip out `shouldRender` as it didn't seem to have a noticeable effect

--- source/index.js
+++ source/index.js
@@ -4,7 +4,6 @@ import isWeekend from './application/isWeekend';
 import partsRemaining from './application/partsRemaining';
 import remainingSeconds from './application/remainingSeconds';
 import scale from './application/scale';
-import shouldRender from './application/shouldRender';
 
 const themeColor = document.querySelector('meta[name="theme-color"]');
 
@@ -18,32 +17,29 @@ const clock = {
 };
 
 const render = (now) => {
+	const happy = isWeekend(now);
 	const parts = partsRemaining(now);
 
-	if (shouldRender(parts)) {
-		const happy = isWeekend(now);
+	const color = new Color({
+		r: scale(parts.hours, happy ? 62 : 104),
+		g: scale(parts.minutes, 59),
+		b: scale(parts.seconds, 59)
+	});
 
-		const color = new Color({
-			r: scale(parts.hours, happy ? 62 : 104),
-			g: scale(parts.minutes, 59),
-			b: scale(parts.seconds, 59)
-		});
+	document.body.classList.toggle('is-dark', color.isDark());
+	document.body.classList.toggle('is-light', color.isLight());
+	document.body.style.backgroundColor = color.hex();
 
-		document.body.classList.toggle('is-dark', color.isDark());
-		document.body.classList.toggle('is-light', color.isLight());
-		document.body.style.backgroundColor = color.hex();
+	themeColor.setAttribute('content', color.hex());
 
-		themeColor.setAttribute('content', color.hex());
+	answer.classList.toggle('answer-yes', happy);
+	answer.classList.toggle('answer-no', !happy);
 
-		answer.classList.toggle('answer-yes', happy);
-		answer.classList.toggle('answer-no', !happy);
+	answer.textContent = happy ? 'Yes!' : 'nope';
 
-		answer.textContent = happy ? 'Yes!' : 'nope';
-
-		clock.hours.textContent = parts.hours.toString().padStart(3, '0');
-		clock.minutes.textContent = parts.minutes.toString().padStart(2, '0');
-		clock.seconds.textContent = parts.seconds.toString().padStart(2, '0');
-	}
+	clock.hours.textContent = parts.hours.toString().padStart(3, '0');
+	clock.minutes.textContent = parts.minutes.toString().padStart(2, '0');
+	clock.seconds.textContent = parts.seconds.toString().padStart(2, '0');
 };
 
 const query = new URL(document.location).searchParams;
@@ -66,5 +62,10 @@ if (query.has('pause')) {
 		render(new Date());
 	}
 } else {
-	setInterval(() => render(new Date()), 1000 / 20);
+	const renderLoop = () => {
+		render(new Date());
+		requestAnimationFrame(renderLoop);
+	};
+
+	requestAnimationFrame(renderLoop);
 }
